e8565aba4618c9186686315ef14e51eb4a4e0291
Sonal-deX <lasinthaattanayake@gmail.com>
2025-05-30 00:16:29 +0530

    refactor: clean up formatting and indentation in EmailModule and EmailService

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -7,33 +7,33 @@ import { ConfigService } from '@nestjs/config';
 
 @Global() // Make EmailService available globally
 @Module({
-  imports: [
-    MailerModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        transport: {
-          host: configService.get<string>('EMAIL_HOST'),
-          port: configService.get<number>('EMAIL_PORT'),
-          secure: configService.get<number>('EMAIL_PORT') === 465, // true for 465, false for 587
-          auth: {
-            user: configService.get<string>('EMAIL_USER'),
-            pass: configService.get<string>('EMAIL_PASSWORD'),
-          },
-        },
-        defaults: {
-          from: `"${configService.get<string>('EMAIL_FROM_NAME')}" <${configService.get<string>('EMAIL_FROM_ADDRESS')}>`,
-        },
-        template: {
-          dir: join(__dirname, 'templates'), // Path to email templates
-          adapter: new HandlebarsAdapter(),
-          options: {
-            strict: true,
-          },
-        },
-      }),
-    }),
-  ],
-  providers: [EmailService],
-  exports: [EmailService],
+    imports: [
+        MailerModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                transport: {
+                    host: configService.get<string>('EMAIL_HOST'),
+                    port: configService.get<number>('EMAIL_PORT'),
+                    secure: configService.get<number>('EMAIL_PORT') === 465, // true for 465, false for 587
+                    auth: {
+                        user: configService.get<string>('EMAIL_USER'),
+                        pass: configService.get<string>('EMAIL_PASSWORD'),
+                    },
+                },
+                defaults: {
+                    from: `"${configService.get<string>('EMAIL_FROM_NAME')}" <${configService.get<string>('EMAIL_FROM_ADDRESS')}>`,
+                },
+                template: {
+                    dir: join(__dirname, 'templates'), // Path to email templates
+                    adapter: new HandlebarsAdapter(),
+                    options: {
+                        strict: true,
+                    },
+                },
+            }),
+        }),
+    ],
+    providers: [EmailService],
+    exports: [EmailService],
 })
-export class EmailModule {}
+export class EmailModule { }
